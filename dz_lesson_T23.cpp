0865eefb730c03e98e52ad343c2971d4f5030019
Krystyna Khondak <kristinakhondak@gmail.com>
2025-07-12 22:37:01 +0300

    exercise 5

--- dz_lesson_T23.cpp
+++ dz_lesson_T23.cpp
@@ -5,6 +5,26 @@ using namespace std;
 int main()
 {
 	// 4
+	//char C;
+	//string S, S0;
+	//cout << "Enter C: ";
+	//cin >> C;
+	//cout << "Enter S: ";
+	//cin.ignore();
+	//getline(cin, S);
+	//cout << "Enter S0: ";
+	//getline(cin, S0);
+	//for (int i = 0; i < S.length(); i++)
+	//{
+	//	if (S[i] == C)
+	//	{
+	//		S.insert(i, " " + S0);
+	//		i += S0.length() + 1;
+	//	}
+	//}
+	//cout << "Result: " << S << endl;
+
+	// 5
 	char C;
 	string S, S0;
 	cout << "Enter C: ";
@@ -18,8 +38,8 @@ int main()
 	{
 		if (S[i] == C)
 		{
-			S.insert(i, " " + S0);
-			i += S0.length() + 1;
+			S.insert(i + 1, S0);
+			i += S0.length(); 
 		}
 	}
 	cout << "Result: " << S << endl;
